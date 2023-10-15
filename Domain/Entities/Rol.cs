e1e6bc472c54e38d294864530e5f7a7ea3e8554e
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-15 13:59:53 -0500

    feat: :sparkles: Improved an existing property
    Se mejoro una propiedad existente

--- Domain/Entities/Rol.cs
+++ Domain/Entities/Rol.cs
@@ -3,6 +3,6 @@ namespace Domain.Entities;
 public class Rol : BaseEntity
 {
     public string Nombre { get; set; }
-    public ICollection<User> Users { get; set; }
+    public ICollection<User> Users { get; set; } = new HashSet<User>();
     public ICollection<UserRol> UserRols { get; set; }
 }
