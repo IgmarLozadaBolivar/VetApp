a352f6db9e20486e6e9c134227440010c2e39fe0
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-14 21:51:39 -0500

    feat: :sparkles: Enabling connections to the database
    Habilitando conexion con la base de datos

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DbAppContext>(options =>
+{
+    options.UseNpgsql("Host=localhost;Database=VetDb;User=postgres;Password=1122809631;");
+});
 
 var app = builder.Build();
 
