125d2e4396c2b1a6eab92c9972d21779ea5918ed
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-15 13:58:59 -0500

    feat: :sparkles: Two properties removed and a new one added
    Se eliminaron dos propiedades y se agrego una nueva

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -5,8 +5,7 @@ public class User : BaseEntity
     public string Mail { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public int IdRolFK { get; set; }
-    public Rol Rol { get; set; }
+    public ICollection<Rol> Rols { get; set; } = new HashSet<Rol>();
     public ICollection<RefreshToken> RefreshTokens { get; set; } = new HashSet<RefreshToken>();
     public ICollection<UserRol> UserRols { get; set; }
     public ICollection<Propietario> Propietarios { get; set; }
