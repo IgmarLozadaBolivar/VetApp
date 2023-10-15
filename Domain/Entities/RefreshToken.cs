e45af4149c2989edc47ca2165d6639f77411a67f
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-15 14:00:20 -0500

    feat: :fire: A property was removed
    Se removio una propiedad

--- Domain/Entities/RefreshToken.cs
+++ Domain/Entities/RefreshToken.cs
@@ -5,7 +5,6 @@ public class RefreshToken : BaseEntity
     public int IdUserFK { get; set; }
     public User User { get; set; }
     public string Token { get; set; }
-    public string TokenRefresh { get; set; }
     public DateTime Expires { get; set; }
     public bool IsExpired => DateTime.UtcNow >= Expires;
     public DateTime Created { get; set; }
