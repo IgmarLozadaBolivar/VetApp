0c84600f75ecf71d79bf2dd01a7b954aa4717776
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-16 09:55:11 -0500

    feat: :sparkles: Added floating window functionality to validate the token
    Se agrego la funcionalidad de ventana flotante para validar el token

--- Front/Js/home.js
+++ Front/Js/home.js
@@ -1,3 +1,45 @@
+Swal.fire({
+    title: 'Ingresa tu token',
+    icon: 'warning',
+    html: `
+        <input type="text" id="token-input" class="swal2-input" placeholder="Ingresa tu token aquí">
+    `,
+    showCancelButton: true,
+    confirmButtonText: 'Aceptar',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    preConfirm: async () => {
+        const token = document.getElementById('token-input').value;
+        try {
+            const response = await fetch('http://localhost:5285/User/validarToken', {
+                method: 'POST',
+                body: JSON.stringify({ token }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                Swal.fire({
+                    title: '¡Token valido!',
+                    text: 'Puedes disfrutar de los servicios de la app!',
+                    icon: 'success',
+                });
+                return token;
+            } else {
+                Swal.showValidationMessage('Token invalido');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    },
+}).then((result) => {
+    if (result.isConfirmed) {
+        const token = result.value;
+    } else if (result.isDismissed) {
+        window.location.href = 'http://127.0.0.1:5500/Front/Html/login.html';
+    }
+});
+
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
