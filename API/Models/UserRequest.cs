a45a610939e47bde0b58482103150e9d14814138
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-15 20:43:45 -0500

    feat: :sparkles: Creation of the request and response model
    Creacion del modelo de solicitud y respuesta

---
+++ API/Models/UserRequest.cs
@@ -0,0 +1,8 @@
+namespace API.Models;
+
+public class UserRequest
+{
+    public string Mail { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ API/Models/UserResponse.cs
@@ -0,0 +1,8 @@
+namespace API.Models;
+
+public class UserResponse
+{
+    public bool Result { get; set; }
+    public string Token { get; set; }
+    public string Msg { get; set; }
+}
