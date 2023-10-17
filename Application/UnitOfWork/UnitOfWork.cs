6bfd9cf3dd8f06e31f329bdd5cadcd68b64e429e
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-17 16:09:22 -0500

    feat: :art: Changes to the class unit of work
    Cambios en la unidad de trabajo de clase

--- Application/UnitOfWork/UnitOfWork.cs
+++ Application/UnitOfWork/UnitOfWork.cs
@@ -1,3 +1,4 @@
+using Application.Repository;
 using Domain.Interfaces;
 using Persistence;
 
@@ -6,22 +7,193 @@ namespace Application.UnitOfWork;
 public class UnitOfWork : IUnitOfWork, IDisposable
 {
     private readonly DbAppContext context;
-    //private ;
+    private VeterinarioRepo _veterinarios;
+    private MedicamentoRepo _medicamentos;
+    private MascotaRepo _mascotas;
+    private PropietarioRepo _propietarios;
+    private MovimientoMedicamentoRepo _movimientoMedicamentos;
+    private ProveedorRepo _proveedores;
+    private CitaRepo _citas;
+    private DetalleMovimientoRepo _detalleMovimiento;
+    private EspecieRepo _especies;
+    private LaboratorioRepo _laboratorios;
+    private RazaRepo _razas;
+    private TipoMovimientoRepo _tipoMovimientos;
+    private TratamientoMedicoRepo __tratamientoMedicos;
+    private UserRepo _users;
 
     public UnitOfWork(DbAppContext _context)
     {
         context = _context;
     }
 
-    /* public  
+    public IVeterinario Veterinarios
     {
-        get{
-            if(== null){
-                = new (context);
+        get
+        {
+            if (_veterinarios == null)
+            {
+                _veterinarios = new VeterinarioRepo(context);
             }
-            return ;
+            return _veterinarios;
         }
-    } */
+    }
+
+    public IMedicamento Medicamentos
+    {
+        get
+        {
+            if (_medicamentos == null)
+            {
+                _medicamentos = new MedicamentoRepo(context);
+            }
+            return _medicamentos;
+        }
+    }
+
+    public IMascota Mascotas
+    {
+        get
+        {
+            if (_mascotas == null)
+            {
+                _mascotas = new MascotaRepo(context);
+            }
+            return _mascotas;
+        }
+    }
+
+    public IPropietario Propietarios
+    {
+        get
+        {
+            if (_propietarios == null)
+            {
+                _propietarios = new PropietarioRepo(context);
+            }
+            return _propietarios;
+        }
+    }
+
+    public IMovimientoMedicamento MovimientoMedicamentos
+    {
+        get
+        {
+            if (_movimientoMedicamentos == null)
+            {
+                _movimientoMedicamentos = new MovimientoMedicamentoRepo(context);
+            }
+            return _movimientoMedicamentos;
+        }
+    }
+
+    public IProveedor Proveedores
+    {
+        get
+        {
+            if (_proveedores == null)
+            {
+                _proveedores = new ProveedorRepo(context);
+            }
+            return _proveedores;
+        }
+    }
+
+    public ICita Citas
+    {
+        get
+        {
+            if (_citas == null)
+            {
+                _citas = new CitaRepo(context);
+            }
+            return _citas;
+        }
+    }
+
+    public IDetalleMovimiento DetalleMovimientos
+    {
+        get
+        {
+            if (_detalleMovimiento == null)
+            {
+                _detalleMovimiento = new DetalleMovimientoRepo(context);
+            }
+            return _detalleMovimiento;
+        }
+    }
+
+    public IEspecie Especies
+    {
+        get
+        {
+            if (_especies == null)
+            {
+                _especies = new EspecieRepo(context);
+            }
+            return _especies;
+        }
+    }
+
+    public ILaboratorio Laboratorios
+    {
+        get
+        {
+            if (_laboratorios == null)
+            {
+                _laboratorios = new LaboratorioRepo(context);
+            }
+            return _laboratorios;
+        }
+    }
+
+    public IRaza Razas
+    {
+        get
+        {
+            if (_razas == null)
+            {
+                _razas = new RazaRepo(context);
+            }
+            return _razas;
+        }
+    }
+
+    public ITipoMovimiento TipoMovimientos
+    {
+        get
+        {
+            if (_tipoMovimientos == null)
+            {
+                _tipoMovimientos = new TipoMovimientoRepo(context);
+            }
+            return _tipoMovimientos;
+        }
+    }
+
+    public ITratamientoMedico TratamientoMedicos
+    {
+        get
+        {
+            if (__tratamientoMedicos == null)
+            {
+                __tratamientoMedicos = new TratamientoMedicoRepo(context);
+            }
+            return __tratamientoMedicos;
+        }
+    }
+
+    public IUser Users
+    {
+        get
+        {
+            if (_users == null)
+            {
+                _users = new UserRepo(context);
+            }
+            return _users;
+        }
+    }
 
     public void Dispose()
     {
