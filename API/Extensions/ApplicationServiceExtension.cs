5b0aec564f4a000a326ad27a620ae58cbaf9845f
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-17 16:11:51 -0500

    feat: :sparkles: Changes were made and new extensions like RateLimiting and Versioning were added
    Se hicieron cambios y se agregaron nuevas extensiones como RateLimiting y Versioning

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -1,3 +1,9 @@
+using Application.UnitOfWork;
+using AspNetCoreRateLimit;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 namespace API.Extensions;
 
 public static class ApplicationServiceExtension
@@ -10,4 +16,47 @@ public static class ApplicationServiceExtension
            .AllowAnyMethod()
            .AllowAnyHeader());
        });
+
+    public static void AddAplicacionServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
+    public static void ConfigureRatelimiting(this IServiceCollection services)
+    {
+        services.AddMemoryCache();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        services.AddInMemoryRateLimiting();
+        services.Configure<IpRateLimitOptions>(options =>
+        {
+            options.EnableEndpointRateLimiting = true;
+            options.StackBlockedRequests = false;
+            options.HttpStatusCode = 429;
+            options.RealIpHeader = "X-Real-IP";
+            options.GeneralRules = new List<RateLimitRule>
+            {
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Period = "10s",
+                    Limit = 2
+                }
+            };
+        });
+    }
+
+    public static void ConfigureApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+
+            //Para ambas versiones
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("ver"),
+                new HeaderApiVersionReader("X-Version")
+            );
+        });
+    }
 }
