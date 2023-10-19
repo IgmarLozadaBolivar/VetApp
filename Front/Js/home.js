7209e7b7b8cdbb1d6b3e1d359b62bf6bae2298a5
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-19 01:55:15 -0500

    style: :bulb: The lines of code were commented, the entire Front-End directory is in a critical process of having to exterminate that directory and only deal with Back-End
    Se comentaron las lineas de codigo, todo el directorio de Front-End esta en proceso critico de tener que exterminar ese directorio y solo ayanar con Back-End

--- Front/Js/home.js
+++ Front/Js/home.js
@@ -40,6 +40,58 @@ Swal.fire({
     }
 });
 
+let botonPlay = document.getElementById("botonPlay");
+botonPlay.addEventListener("click", function (e) {
+    e.preventDefault();
+    Consulta1();
+});
+
+/* const urlAuth = "http://localhost:5285/api/Veterinario/cirujanosVasculares2";
+const headers = new Headers({ 'Content-Type': 'application/json' });
+
+async function Consulta1() {
+    const config = {
+        method: 'GET',
+        headers: headers,
+    }
+
+    try {
+        const response = await fetch(urlAuth, config);
+
+        if (response.ok) {
+            const result = await response.json();
+
+            const nombre = result[0].Nombre;
+            const email = result[0].Email;
+            const telefono = result[0].Telefono;
+
+            Swal.fire({
+                title: '¡Bienvenido!',
+                text: 'Deseas realizar esta consulta?',
+                icon: 'question',
+                confirmButtonText: 'Si',
+                cancelButtonText: 'No',
+                showCancelButton: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+            });
+
+            if (result) {
+                Swal.fire({
+                    title: '¡Resultado!',
+                    html: `<p>Nombre: ${nombre}</p><p>Email: ${email}</p><p>Teléfono: ${telefono}</p>`,
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                });
+            }
+        } else {
+            alert("Autenticación fallida. Verifique sus credenciales o regístrese.");
+        }
+    } catch (error) {
+        console.error("Error al realizar la Autenticación:", error);
+    }
+} */
+
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
